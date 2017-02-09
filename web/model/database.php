335eb146fb4318a03e5e4225ad4375b314beff43
JONBONIN\Jonathan <jon.e.bonin@gmail.com>
2017-02-09 10:34:22 -0700

    update file to connect with Heroku;

--- web/model/database.php
+++ web/model/database.php
@@ -1,15 +1,29 @@
 <?php
 
-try
-{
-  $user = 'percy';
-  $password = 'w6O1FbMBdhi4nJWS2LK0';
-  $db = new PDO('pgsql:host=127.0.0.1;dbname=percmap', $user, $password);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
+// default Heroku Postgres configuration URL
+$dbUrl = getenv('DATABASE_URL');
+
+if (empty($dbUrl)) {
+ // example localhost configuration URL with postgres username and a database called cs313db
+ $dbUrl = "postgres://percy:w6O1FbMBdhi4nJWS2LK0@localhost:5432/percmap";
 }
 
-//w6O1FbMBdhi4nJWS2LK025
+$dbopts = parse_url($dbUrl);
+
+print "<p>$dbUrl</p>\n\n";
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+
+try {
+ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+catch (PDOException $ex) {
+ print "<p>error: $ex->getMessage() </p>\n\n";
+ die();
+}
